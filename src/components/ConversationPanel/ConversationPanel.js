73587e129f13d8e757dd55385670ed849440866b
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-19 10:12:47 +0200

    visualize file content inside text area in ConversationPanel

--- src/components/ConversationPanel/ConversationPanel.js
+++ src/components/ConversationPanel/ConversationPanel.js
@@ -1,10 +1,23 @@
+import Form from "react-bootstrap/Form"
+
+import useFile from "hooks/useFile"
+
 import "./ConversationPanel.scss"
 
 const ConversationPanel = () => {
+  const fileContent = useFile()
+
   return (
     <div className="conversation-panel">
       <div className="left-col">Left Column</div>
-      <div className="right-col">Right Column</div>
+      <div className="right-col m-2">
+        <Form.Control
+          as="textarea"
+          defaultValue={fileContent}
+          disabled
+          className="h-100"
+        />
+      </div>
     </div>
   )
 }
